153da029ded031579d9bfdd9c709dfc03dd271a3
Patrick Altman <paltman@gmail.com>
2020-06-02 14:39:30 -0500

    Only show selected token in default mode

--- src/reader/components/ReaderToken.vue
+++ src/reader/components/ReaderToken.vue
@@ -62,12 +62,15 @@
         if (!this.selectedToken) {
           return false;
         }
-        const { entities, veRef } = this.selectedToken;
-        const entity = entities[0];
-        return (
-          veRef === this.token.veRef ||
-          (entity && entity === this.token.entities[0])
-        );
+        if (this.namedEntities) {
+          const { entities, veRef } = this.selectedToken;
+          const entity = entities[0];
+          return (
+            veRef === this.token.veRef ||
+            (entity && entity === this.token.entities[0])
+          );
+        }
+        return this.selectedToken.veRef === this.token.veRef;
       },
       selectedToken() {
         return this.$store.state[MODULE_NS].selectedToken;
