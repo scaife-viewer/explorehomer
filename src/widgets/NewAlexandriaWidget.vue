406b4ba76ca8ba3ed0e8773d44500e912a3577c9
Jacob Wegner <jwegner@eldarion.com>
2020-07-02 15:20:38 -0500

    add docstrings, rename functions

--- src/widgets/NewAlexandriaWidget.vue
+++ src/widgets/NewAlexandriaWidget.vue
@@ -1,4 +1,16 @@
 <script>
+  /*
+  This widget contains customizations for Explore Homer that handle
+  special-case queries tothe New Alexandria Commentary API.
+
+  In the future, we may want to take this type of customization
+  into consideration when implementing hooks / extensibility points
+  within `scaife-widgets`.
+
+  We may also find that the Vue Composition API
+  (https://composition-api.vuejs.org/) lends itself to these types of
+  customizations.
+  */
   import gql from 'graphql-tag';
 
   import WIDGETS_NS, {
@@ -12,14 +24,18 @@
       displayName: 'New Alexandria Commentary',
     },
     data: () => ({
-      resolvedPassageTextParts: [],
+      passageTextParts: [],
       healedPassage: '',
     }),
     computed: {
       originalPassage() {
+        // the passage retrieved from the store
         return this.$store.getters[`${WIDGETS_NS}/passage`];
       },
       passage() {
+        // overrides `NewAlexandriaWidget.computed.passage`
+        // so that only the healedPassage is passed to NAC
+        // via the `passage` watcher
         return this.healedPassage;
       },
     },
@@ -27,6 +43,8 @@
       originalPassage: {
         immediate: true,
         handler() {
+          // special-case handling for the Folios version of
+          // the Iliad
           if (
             this.originalPassage.version ===
             'urn:cts:greekLit:tlg0012.tlg001.msA-folios:'
@@ -37,32 +55,48 @@
           }
         },
       },
-      resolvedPassageTextParts() {
-        const refs = [];
-        refs.push(this.resolvedPassageTextParts.slice(0, 1)[0].ref);
-        if (this.resolvedPassageTextParts.length > 1) {
-          refs.push(this.resolvedPassageTextParts.slice(-1)[0].ref);
-        }
-        this.extractHealedRefs(refs);
+      passageTextParts() {
+        // after we have retrieved passage text parts,
+        // extract the refs and populate healedPassage
+        const refs = this.extractPassageTextPartRefs();
+        const healedRefs = this.extractHealedRefs(refs);
+        this.updateHealedPassage(healedRefs);
       },
     },
     methods: {
+      hasLinesInRef(ref) {
+        // good: 2r.1.1
+        // bad: 12r, 12r.1
+        return ref.split('.').length === 3;
+      },
+      healFolioURN() {
+        const refs = this.originalPassage.reference.split('-');
+        if (this.hasLinesInRef(refs[0])) {
+          const healedRefs = this.extractHealedRefs(refs);
+          this.updateHealedPassage(healedRefs);
+        } else {
+          this.fetchPassageTextParts();
+        }
+      },
       extractHealedRefs(refs) {
+        // pops the folio off of refs,
+        // e.g. 12r.1.1-12r.1.7 --> 1.1-1.7
         const healedRefs = [];
         refs.forEach(ref => {
           const refParts = ref.split('.');
-          if (refParts.length > 1) {
-            healedRefs.push(refParts.slice(1).join('.'));
-          } else {
-            this.getPassageTextParts();
-          }
+          healedRefs.push(refParts.slice(1).join('.'));
         });
-        if (healedRefs.length > 0) {
-          const refPart = healedRefs.join('-');
+        return healedRefs;
+      },
+      updateHealedPassage(refs) {
+        if (refs.length > 0) {
+          const refPart = refs.join('-');
           this.healedPassage = `${this.originalPassage.version}${refPart}`;
         }
       },
-      getPassageTextParts() {
+      fetchPassageTextParts() {
+        // retrieves the lowest text parts for a folio level reference,
+        // e.g. 12r --> 12r.1.1-12.r.1.25
         const query = gql`
           {
             passageTextParts(
@@ -78,18 +112,23 @@
           }
         `;
         this.$gql(query).then(data => {
-          this.resolvedPassageTextParts = data.passageTextParts.edges.map(
-            edge => {
-              const { node } = edge;
-              return node;
-            },
-          );
+          this.passageTextParts = data.passageTextParts.edges.map(edge => {
+            const { node } = edge;
+            return node;
+          });
         });
         return query;
       },
-      healFolioURN() {
-        const refs = this.originalPassage.reference.split('-');
-        this.extractHealedRefs(refs);
+      extractPassageTextPartRefs() {
+        // extracts the ref(s) from the text parts
+        const refs = [];
+        // start ref
+        refs.push(this.passageTextParts.slice(0, 1)[0].ref);
+        if (this.passageTextParts.length > 1) {
+          // end ref
+          refs.push(this.passageTextParts.slice(-1)[0].ref);
+        }
+        return refs;
       },
     },
   };
