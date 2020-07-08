16024cd439c4b2c9a39c680f13110d624a071ed9
Patrick Altman <paltman@gmail.com>
2020-07-08 14:32:35 -0500

    Use same separator style as folios

--- src/components/SelectableEntityMap.vue
+++ src/components/SelectableEntityMap.vue
@@ -37,6 +37,21 @@
   };
 </script>
 
+<style lang="scss">
+  .map-direction-horizontal {
+    .map {
+      padding-left: 1rem;
+      border-left: 1px solid $gray-300;
+    }
+  }
+  .map-direction-vertical {
+    .map {
+      padding-top: 1rem;
+      border-top: 1px solid $gray-300;
+    }
+  }
+</style>
+
 <style lang="scss" scoped>
   .selectable-entity-map {
     height: 100%;
