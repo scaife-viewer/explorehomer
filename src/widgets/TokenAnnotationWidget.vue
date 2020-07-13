56601b6293b5aee1634887c5c35a24bf39585841
Patrick Altman <paltman@gmail.com>
2020-07-13 12:38:10 -0500

    Convert token annotations widget

--- src/widgets/TokenAnnotationWidget.vue
+++ src/widgets/TokenAnnotationWidget.vue
@@ -1,6 +1,8 @@
 <template>
   <table class="selected-tokens">
-    <tr v-if="tokens.length === 0"><EmptyMessage /></tr>
+    <tr v-if="tokens.length === 0">
+      <EmptyMessage />
+    </tr>
     <tr colspan="3" v-else-if="selectedToken">
       <a href @click.prevent="onClear">Clear Filter</a>
     </tr>
@@ -38,12 +40,39 @@
           ? new URN(this.$route.query.urn)
           : this.$store.getters[`${MODULE_NS}/firstPassageUrn`];
       },
-      // TODO: Dedupe from Reader.vue
-      gqlQuery() {
-        if (this.urn) {
-          return gql`
-          {
-            passageTextParts(reference: "${this.urn.absolute}") {
+      tokens() {
+        const selectedFilter = edge => {
+          return (
+            this.selectedToken === null ||
+            this.selectedToken.veRef === edge.node.veRef
+          );
+        };
+
+        if (this.lines && this.lines.length > 0) {
+          const tokens = this.lines
+            .map(line => {
+              const { edges } = line.tokens;
+              return edges.filter(selectedFilter).map(edge => {
+                const token = edge.node;
+                return {
+                  veRef: token.veRef,
+                  value: token.wordValue,
+                  lemma: token.lemma,
+                  tag: token.tag,
+                };
+              });
+            })
+            .flat();
+          return [...new Set(tokens)];
+        }
+        return [];
+      },
+    },
+    apollo: {
+      lines: {
+        query: gql`
+          query Annotations($urn: String!) {
+            passageTextParts(reference: $urn) {
               edges {
                 node {
                   id
@@ -62,42 +91,13 @@
               }
             }
           }
-          `;
-        }
-        return null;
-      },
-      // TODO: Dedupe from Reader.vue
-      lines() {
-        return this.gqlData
-          ? this.gqlData.passageTextParts.edges.map(line => line.node)
-          : [];
-      },
-      tokens() {
-        const selectedFilter = edge => {
-          return (
-            this.selectedToken === null ||
-            this.selectedToken.veRef === edge.node.veRef
-          );
-        };
-
-        if (this.lines.length > 0) {
-          const tokens = this.lines
-            .map(line => {
-              const { edges } = line.tokens;
-              return edges.filter(selectedFilter).map(edge => {
-                const token = edge.node;
-                return {
-                  veRef: token.veRef,
-                  value: token.wordValue,
-                  lemma: token.lemma,
-                  tag: token.tag,
-                };
-              });
-            })
-            .flat();
-          return [...new Set(tokens)];
-        }
-        return [];
+        `,
+        variables() {
+          return { urn: this.urn.absolute };
+        },
+        update(data) {
+          return data.passageTextParts.edges.map(line => line.node);
+        },
       },
     },
   };
