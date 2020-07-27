b5d1feb744a0d65c57d0dedb61a91f18c195d3cf
Patrick Altman <paltman@gmail.com>
2020-07-27 13:52:13 -0500

    Implement healing

--- src/widgets/PassageReferenceWidget.vue
+++ src/widgets/PassageReferenceWidget.vue
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+  import gql from 'graphql-tag';
   import WIDGETS_NS from '@scaife-viewer/scaife-widgets';
 
   export default {
@@ -44,14 +45,29 @@
         }
       },
       handleKeyUp() {
-        this.$router.push({
-          to: 'reader',
-          query: {
-            urn: this.reference
-              ? `${this.passage.version}${this.reference}`
-              : this.firstPassageUrn.toString(),
-          },
-        });
+        const urn = `${this.passage.version}${this.reference}`;
+        this.$apollo
+          .query({
+            query: gql`
+              query ValidateReference($urn: String!) {
+                passageTextParts(reference: $urn) {
+                  metadata {
+                    healedPassage
+                    humanReference
+                  }
+                }
+              }
+            `,
+            variables: { urn },
+            skip: this.reference === '',
+          }).then(data => {
+            console.log('Passage Data', data.data.passageTextParts.metadata);
+            const { healedPassage } = data.data.passageTextParts.metadata;
+            const ref = healedPassage || urn;
+            this.$router.push({ to: 'reader', query: { urn: ref } });
+          }).catch(error => {
+            console.log('Passage Error', error);
+          });
       },
       handleClick(e) {
         const el = e.currentTarget;
